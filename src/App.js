ebdcebafb9a7d05ac24616b2ae4e3a26f1e0308e
Mason Brown <masonbrown@Masons-MacBook-Pro.local>
2017-12-06 18:35:45 -0500

    initial folder structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './reset.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
